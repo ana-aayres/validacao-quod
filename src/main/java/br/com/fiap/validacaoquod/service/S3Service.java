bb8369b8153251425e7f7d0bff0f3422a3e76d18
Pablo Machado <sotim182@gmail.com>
2025-04-30 16:59:27 -0300

    S3

---
+++ src/main/java/br/com/fiap/validacaoquod/service/S3Service.java
@@ -0,0 +1,34 @@
+package br.com.fiap.validacaoquod.service;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+@Service
+public class S3Service {
+    private final S3Client s3Client;
+
+    @Value("${AWS_BUCKET_NAME}")
+    private String bucketName;
+
+    public S3Service(S3Client s3Client) {
+        this.s3Client = s3Client;
+    }
+
+    public String uploadFile(MultipartFile file, String fileName) throws IOException {
+        PutObjectRequest request = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(fileName)
+                .build();
+
+        s3Client.putObject(request, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
+
+        return "https://" + bucketName + ".s3.amazonaws.com/" + fileName;
+    }
+}
