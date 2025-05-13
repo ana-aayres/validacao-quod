f111591ebdb1074ebc407da6921a5149035a5d2a
Pablo Machado <sotim182@gmail.com>
2025-05-13 19:06:11 -0300

    frontend

--- frontend-quod/src/services/api.js
+++ frontend-quod/src/services/api.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
 
-export const Cadastrar = async (dados, documento, selfie1, selfie2) => {
+export const Cadastrar = async (dados, documento, selfie1, selfie2, fingerprint) => {
   const formData = new FormData();
   
   formData.append('dados', JSON.stringify(dados));
   formData.append('documento', documento);  // Enviar o arquivo diretamente
   formData.append('selfie1', selfie1);
   formData.append('selfie2', selfie2);
+  formData.append('fingerprint', fingerprint);
+
 
   try {
     const response = await axios.post('http://localhost:8080/api/notificacoes/fraude', formData, {
@@ -17,3 +19,6 @@ export const Cadastrar = async (dados, documento, selfie1, selfie2) => {
     console.error('Erro ao salvar fraude:', error);
   }
 };
+
+
+
